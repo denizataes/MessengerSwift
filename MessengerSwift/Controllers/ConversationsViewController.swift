181a2e50d8e163ee1302b398f459df9969253702
Deniz Ata EŞ <85442526+denizataes@users.noreply.github.com>
2023-01-26 00:00:41 +0300

    Logout implemented tabbar, navigationbar implemented some bugfixed

--- MessengerSwift/Controllers/ConversationsViewController.swift
+++ MessengerSwift/Controllers/ConversationsViewController.swift
@@ -13,7 +13,7 @@ class ConversationsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        view.backgroundColor = .red
+        //view.backgroundColor = .red
     }
     
     override func viewDidAppear(_ animated: Bool) {
